d45a42f9b893a70cd52a1f74cf246adc55609794
Richard Barnes-Webb <Richard.Barnes-Webb@paconsulting.com>
2021-07-22 08:35:10 +0100

    Committing fixes in the dark, seriously, who killed my power!?

--- tfl_WestBarnesLevelCrossing.py
+++ tfl_WestBarnesLevelCrossing.py
@@ -10,15 +10,15 @@
 import requests
 from datetime import datetime, timedelta
 
-STATIONNAME     =   'West Barnes Level Crossing'
-                    #  "Stop E"    "Stop P"
+                    #  "Stop E"      "Stop P"
 STOPPOINT_IDS   =   ['490014399N', '490014399S'] 
+STATIONNAME     =   'West Barnes Level Crossing'
 
 print(f"Arrival times at {STATIONNAME} (time now: {datetime.now().strftime('%H:%M')})")
 for stopPoint in STOPPOINT_IDS:
     arrivals = requests.get(f"https://api.tfl.gov.uk/StopPoint/{stopPoint}/Arrivals").json()
     for arrival in sorted(arrivals, key=lambda k: k['timeToStation']):
-        print (f"{arrival['lineName']} to {arrival['destinationName']:30} - due: {(datetime.now() + timedelta(seconds=arrival['timeToStation'])).strftime('%H:%M')}")
+        print (f"{arrival['lineName']:4} to {arrival['destinationName']:20} - due: {(datetime.now() + timedelta(seconds=arrival['timeToStation'])).strftime('%H:%M')}")
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #//EOF
